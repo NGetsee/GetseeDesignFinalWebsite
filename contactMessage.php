41a02d95ffa6e33dae59b468db688fb9166593e3
NGetsee <123643158+NGetsee@users.noreply.github.com>
2023-12-28 20:58:51 +0000

    Update Form Name to allow ' and db message update

--- contactMessage.php
+++ contactMessage.php
@@ -13,6 +13,8 @@ $txtName = '';
 $txtEmail = '';
 $message = '';
 $phoneNumber = '';
+$emailMessage = '';
+$subject= '';
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -20,17 +22,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $txtName = $_POST['txtName'];
     $txtEmail = $_POST['txtEmail'];
     $phoneNumber =$_POST['phone_number'];
-    $services =$_POST['services'];
+    $emailMessage = $_POST['message'];
+    $subject= $_POST['msg_subject'];
 
-    // Database insert SQL code 
-    $sql = "INSERT INTO `tbl_homepagecontact` (`Id`, `fldName`, `fldEmail`,`fldPhone`,`fldService`) VALUES (null, ?, ?, ?, ?)";
+    // Database insert SQL code (you should use prepared statements for security)
+   $sql = "INSERT INTO `tbl_contactmessage` (`Id`,`fldName`, `fldEmail`, `fldPhone`,`fldSubject`,`fldMessage` ) VALUES (null,?, ?, ?, ?, ?)";
 
-    // avoid SQL injection by using this code
+    // Use prepared statements to avoid SQL injection0
     $stmt = mysqli_prepare($con, $sql);
-    mysqli_stmt_bind_param($stmt, "ssss", $txtName, $txtEmail,$phoneNumber, $services);
+    mysqli_stmt_bind_param($stmt, "sssss", $txtName, $txtEmail,$phoneNumber,$subject,$emailMessage);
     
     if (mysqli_stmt_execute($stmt)) {
-        
+        // Database records inserted successfully
 
         // Create a new PHPMailer instance for sending user confirmation
         $userEmail = new PHPMailer(true);
@@ -42,9 +45,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $userEmail->Host = 'smtp.outlook.com'; 
             $userEmail->SMTPAuth = true;
             $userEmail->Username = 'NGiblin1988@outlook.com'; // My email address
-            $userEmail->Password = 'gETSEE23*'; // MY email password
-            $userEmail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Usisng 'tls' 
-            $userEmail->Port = 587; // my appropriate port
+            $userEmail->Password = 'gETSEE23*'; // My email password
+            $userEmail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Using 'tls' 
+            $userEmail->Port = 587; // Use the appropriate port
 
             // Sender and recipient (to the person filling out the form)
             $userEmail->setFrom('NGiblin1988@outlook.com', 'Nicola Giblin');
@@ -69,28 +72,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 // Server settings for notification email
                 $notificationEmail->isSMTP();
                 $notificationEmail->SMTPDebug = SMTP::DEBUG_OFF; // Set to SMTP::DEBUG_SERVER for debugging
-                $notificationEmail->Host = 'smtp.outlook.com';
+                $notificationEmail->Host = 'smtp.outlook.com'; 
                 $notificationEmail->SMTPAuth = true;
                 $notificationEmail->Username = 'NGiblin1988@outlook.com'; // My email address
                 $notificationEmail->Password = 'gETSEE23*'; // My email password
                 $notificationEmail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Using 'tls' 
                 $notificationEmail->Port = 587; 
 
-                // Sender and recipient (notification to yourself)
+                // Sender and recipient (notification to meyself)
                 $notificationEmail->setFrom('NGiblin1988@outlook.com', 'Nicola Giblin');
-                $notificationEmail->addAddress('NGiblin1988@outlook.com', 'Nicola Giblin'); //  My email and name
+                $notificationEmail->addAddress('NGiblin1988@outlook.com', 'Nicola Giblin'); // My email and name
 
-                // Email content (notification to me)
+                // Email content (notification to myself)
                 $notificationEmail->isHTML(true);
                 $notificationEmail->Subject = 'Form Submission Notification';
-               $notificationEmail->Body = 'A form submission has been received from ' . $txtName . ' (' . $txtEmail . ') with phone number: ' . $phoneNumber;
- 
+               $notificationEmail->Body = 'A form submission has been received from ' . $txtName . ' (' . $txtEmail . ') with phone number: ' . $phoneNumber . ' and subject: ' . $emailMessage;
 
-                // Send the notification email to me
                 $notificationEmail->send();
                 
                 // Redirect to the homepage
-                header('Location: index.html');
+                header('Location: index.html'); 
                 exit; // Exit to prevent further script execution
 
             } catch (Exception $e) {
